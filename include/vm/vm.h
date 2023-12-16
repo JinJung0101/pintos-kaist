77c87968b08a053f16698ee1351d1437e09950db
JinJung0101 <qwe5344641@gmail.com>
2023-12-16 16:18:03 +0900

    supplemental_page_table 구조체 수정중

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include "threads/palloc.h"
 #include <hash.h>
+#include "lib/kernel/hash.h"
 
 enum vm_type {
 	/* page not initialized */
@@ -47,6 +48,7 @@ struct page {
 	struct frame *frame;   /* Back reference for frame */
 
 	/* Your implementation */
+	struct hash_elem page_hash_elem;
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
@@ -86,10 +88,11 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
-	
+	struct hash spt_hash;
 };
 
 #include "threads/thread.h"
+
 void supplemental_page_table_init (struct supplemental_page_table *spt);
 bool supplemental_page_table_copy (struct supplemental_page_table *dst,
 		struct supplemental_page_table *src);
